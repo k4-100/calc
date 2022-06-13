52e44f9548d2f5a479134e5d22576a8540a41185
k4-100 <k4-100@protonmail.com>
2022-06-13 19:02:45 +0200

    Barebone implementation  of UtilityBelt

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import CustomTable from './components/CustomTable/index'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './dist/css/compiled/main.css';
+import UtilityBelt from './components/UtilityBelt';
 
 const App: React.FC = () => {
   return (
     <div className="App">
         <h1>
+            <UtilityBelt />
             <CustomTable />
         </h1>
     </div>
