e71fb62e4623a8fe8533e70e4e56fb8c42b765c1
k4-100 <k4-100@protonmail.com>
2022-06-25 15:10:40 +0200

    Dummy table that works

--- src/components/CustomTable/index.js
+++ src/components/CustomTable/index.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import _ from 'lodash'
-import { useTable } from '@tanstack/react-table'
+import { useTable } from 'react-table'
 /**
  * 
  * @returns Table with cells 
@@ -52,11 +52,121 @@ const CustomTable = () =>{
       // e: any
       ) =>{}
 
-    return(
-      <h1>
-        srw
-      </h1>
-    )
+    // return(
+    //   <h1>
+    //     srw
+    //   </h1>
+    // )
+
+
+ 
+
+ const {
+
+   getTableProps,
+
+   getTableBodyProps,
+
+   headerGroups,
+
+   rows,
+
+   prepareRow,
+
+ } = tableInstance
+
+ 
+
+ return (
+
+   // apply the table props
+
+   <table {...getTableProps()}>
+
+     <thead>
+
+       {// Loop over the header rows
+
+       headerGroups.map(headerGroup => (
+
+         // Apply the header row props
+
+         <tr {...headerGroup.getHeaderGroupProps()}>
+
+           {// Loop over the headers in each row
+
+           headerGroup.headers.map(column => (
+
+             // Apply the header cell props
+
+             <th {...column.getHeaderProps()}>
+
+               {// Render the header
+
+               column.render('Header')}
+
+             </th>
+
+           ))}
+
+         </tr>
+
+       ))}
+
+     </thead>
+
+     {/* Apply the table body props */}
+
+     <tbody {...getTableBodyProps()}>
+
+       {// Loop over the table rows
+
+       rows.map(row => {
+
+         // Prepare the row for display
+
+         prepareRow(row)
+
+         return (
+
+           // Apply the row props
+
+           <tr {...row.getRowProps()}>
+
+             {// Loop over the rows cells
+
+             row.cells.map(cell => {
+
+               // Apply the cell props
+
+               return (
+
+                 <td {...cell.getCellProps()}>
+
+                   {// Render the cell contents
+
+                   cell.render('Cell')}
+
+                 </td>
+
+               )
+
+             })}
+
+           </tr>
+
+         )
+
+       })}
+
+     </tbody>
+
+   </table>
+
+ )
+
+
+
 }
 
 export default CustomTable
