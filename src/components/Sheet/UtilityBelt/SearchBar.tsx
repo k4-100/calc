f84a807367b861a1c2cc0799145ef0c060435f36
k4-100 <k4-100@protonmail.com>
2022-07-10 22:20:14 +0200

    this time with files

---
+++ src/components/Sheet/UtilityBelt/SearchBar.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import _ from "lodash";
+import { useGlobalContext } from "../../../context";
+import { TableClass, SheetClass } from "../../../utility/Classes";
+/**
+ *
+ * @returns Search Bar
+ */
+const SearchBar: React.FC = () => {
+  const { sheet, setSheet } = useGlobalContext();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const tableIndex = 0;
+
+  /**
+   *
+   * @param e on change event
+   */
+  const handleTextChange = (e: any) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchButtonClick = () => {
+    if (searchQuery !== "") {
+      let tableChanged = false;
+      const _table: TableClass = _.cloneDeep(sheet.tables[0]);
+      _table.cells.forEach((row) =>
+        row.forEach((cell) => {
+          if (cell.wasFound) cell.wasFound = false;
+          if (String(cell.value).includes(searchQuery)) {
+            cell.wasFound = true;
+            if (!tableChanged) tableChanged = true;
+          }
+        })
+      );
+      const _sheet: SheetClass = _.cloneDeep(sheet);
+      _sheet.tables[tableIndex] = _table;
+      setSheet!(_sheet);
+    }
+  };
+  return (
+    <div className="SearchBar">
+      <div className="d-flex m-2">
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          onClick={() => handleSearchButtonClick()}
+        >
+          Search
+        </button>
+        <input
+          className="form-control w-25"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={searchQuery}
+          onChange={(e) => handleTextChange(e)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/Sheet/UtilityBelt/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+/**
+ *
+ * @returns Belt with Utilities
+ */
+const UtilityBelt: React.FC = () => {
+  return (
+    <div className="UtilityBelt">
+      <SearchBar />
+    </div>
+  );
+};
+
+export default UtilityBelt;
