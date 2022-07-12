492c078ab425bf41d81812c579595f9549c9380f
k4-100 <k4-100@protonmail.com>
2022-07-12 20:23:25 +0200

    Implemted adding tables to the sheet

--- src/components/Sheet/SheetBar.tsx
+++ src/components/Sheet/SheetBar.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { useGlobalContext } from "../../context";
 import SheetBarButton from "./SheetBarButton";
+import _ from "lodash";
+import { TableClass } from "../../utility/Classes";
 
 const SheetBar = () => {
-  const { sheet } = useGlobalContext();
+  const { sheet, setSheet } = useGlobalContext();
+
+  /**
+   * adds new table into sheet
+   */
+  const handleTableAdd = () => {
+    const _sheet = _.cloneDeep(sheet);
+    _sheet.tables.push(new TableClass(3, 4));
+    setSheet!(_sheet);
+  };
   return (
     <div className="SheetBar">
       <div className="d-flex">
         <div className="d-flex">
           {sheet.tables.length &&
-            sheet.tables.map((_, i) => <SheetBarButton key={i} />)}
-          {/* <SheetBarButton /> */}
+            sheet.tables.map((_, i) => (
+              <SheetBarButton name={`${i + 1}`} key={i} />
+            ))}
         </div>
-        <button type="button" className="btn btn-success ms-auto me-2">
+        <button
+          type="button"
+          className="btn btn-success ms-auto me-2"
+          onClick={() => handleTableAdd()}
+        >
           +
         </button>
       </div>
--- src/components/Sheet/SheetBarButton.tsx
+++ src/components/Sheet/SheetBarButton.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import MdCancel from 'md'
+
 /**
  *
  * @returns SheetBarButton
  */
-const SheetBarButton: React.FC = () => {
+const SheetBarButton: React.FC<{ name: string }> = ({ name }) => {
   return (
     <div className="SheetBarButton h-100 mx-2">
-      <button className="btn btn-primary px-5 rounded-0">1</button>
+      <button className="btn btn-primary px-5 rounded-0">{name}</button>
       <button className="btn btn-danger  rounded-0">X</button>
     </div>
   );
