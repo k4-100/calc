d9bd37c16ae7927e905f123fbac2ac287b15e6b3
k4-100 <k4-100@protonmail.com>
2022-07-14 19:57:02 +0200

    Added routing and new component: Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/compiled/main.css";
 import Sheet from "./components/Sheet";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Sheet />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="calc" element={<Sheet />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
