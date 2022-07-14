cbf67d3589f0c6d0557293d53157eb9dcd61bf49
k4-100 <k4-100@protonmail.com>
2022-07-14 21:03:52 +0200

    finished barebone Navbar/header

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AiOutlineCalculator } from "react-icons/ai";
+import { GrNotes, GrImage } from "react-icons/gr";
+import { CgNotes, CgImage } from "react-icons/cg";
 const Header = () => {
   return (
     <header className="p-1">
       <Navbar className="px-3 py-0" bg="dark" variant="dark">
         <Navbar.Brand href="#home">SHEET</Navbar.Brand>
-        <div className="vr text-info p-4"></div>
+        <div className="vr text-light px-3 "></div>
         <Nav className="me-auto">
-          <Link className="nav-link" to="/calc">
-            Calc
+          <Link className="nav-link p-0 px-1" to="/calc">
+            <AiOutlineCalculator className="h1 h-100" />
           </Link>
-          <Link className="nav-link" to="/calc">
-            Write
+          <Link className="nav-link p-0 px-1" to="/calc">
+            <CgNotes className=" h1 h-100" />
           </Link>
-          <Link className="nav-link" to="/calc">
-            Pres
+          <Link className="nav-link p-0 px-1" to="/calc">
+            <CgImage className="h1 h-100" />
           </Link>
         </Nav>
       </Navbar>
