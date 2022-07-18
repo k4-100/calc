1c52f60704b3873763499b9469661e362bb4aa92
k4-100 <k4-100@protonmail.com>
2022-07-18 21:53:22 +0200

    Added button to utility to return to previous page

--- src/components/Sheet/UtilityBelt/index.tsx
+++ src/components/Sheet/UtilityBelt/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import SearchBar from "./SearchBar";
+
 /**
  *
  * @returns Belt with Utilities
  */
 const UtilityBelt: React.FC = () => {
+  let navigate = useNavigate();
   return (
-    <div className="UtilityBelt">
+    <div className="UtilityBelt d-flex">
+      <Button
+        className="px-3 my-1"
+        onClick={() => navigate("../", { replace: true })}
+      >
+        <AiOutlineArrowLeft fontSize={"2rem"} />
+      </Button>
       <SearchBar />
     </div>
   );
