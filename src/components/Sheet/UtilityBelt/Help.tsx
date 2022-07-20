2ac3fbb104bf30230ddaf8f27be974e0885d4a3d
k4-100 <k4-100@protonmail.com>
2022-07-20 20:39:42 +0200

    Finished basic design for Help.tsx

--- src/components/Sheet/UtilityBelt/Help.tsx
+++ src/components/Sheet/UtilityBelt/Help.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Button, ListGroup } from "react-bootstrap";
+import { ImCross } from "react-icons/im";
 const Help: React.FC = () => {
   return (
     <div
@@ -9,7 +10,31 @@ const Help: React.FC = () => {
         backgroundColor: "hsla(0,0%,15%, 0.80)",
       }}
     >
-      <div className="container  bg-dark h-75 my-5 d-block">ssa</div>
+      <div className="container  bg-dark h-75 my-5 d-block">
+        <div className="d-flex justify-content-between p-2 py-3">
+          <h1 className="text-light">How to Guide: </h1>
+          <Button>
+            <ImCross />
+          </Button>
+        </div>
+
+        <ListGroup className="px-2 ">
+          <ListGroup.Item>
+            To change text, click twice on a cell and start typing, and save by
+            clicking Enter/clicking somewhere else.
+          </ListGroup.Item>
+          <ListGroup.Item>
+            To perform calculation, begin by typing `=`, for example: =10-5
+          </ListGroup.Item>
+          <ListGroup.Item>
+            You can access cell for use in mathematical expression by its "name"
+            (for example: A1, B3, C2), by typing `=` before it (like: =A2, =B3).
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Exit this menu by pressing X in a top-right corner.{" "}
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
