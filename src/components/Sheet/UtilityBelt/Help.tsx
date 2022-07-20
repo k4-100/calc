07b0346c2d37c2718c274dead45696613ab4fcd9
k4-100 <k4-100@protonmail.com>
2022-07-20 20:05:09 +0200

    Started working on basic structure of  Help

--- src/components/Sheet/UtilityBelt/Help.tsx
+++ src/components/Sheet/UtilityBelt/Help.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Help: React.FC = () => {
-  return <div className="Help">words</div>;
+  return (
+    <div
+      className="Help w-100 h-100 position-fixed top-0"
+      style={{
+        zIndex: "1000",
+        backgroundColor: "hsla(0,0%,15%, 0.80)",
+      }}
+    >
+      <div className="container  bg-dark h-75 my-5 d-block">ssa</div>
+    </div>
+  );
 };
 
 export default Help;
