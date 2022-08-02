725659b028140e2dccee5198e212ffc9eaef779e
k4-100 <k4-100@protonmail.com>
2022-08-02 22:26:16 +0200

    Added stuff in a middle

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 import Header from "./Header";
 import { grey } from "@mui/material/colors";
@@ -37,7 +37,57 @@ const Home: React.FC = () => {
         </Typography>
         <Button>Read more</Button>
       </Box>
-      <div className="bg-dark py-1 w-25 mr-3 mt-3"></div>
+      <Grid
+        container
+        columns={{ xs: 12 }}
+        sx={{
+          mt: 3,
+          ml: 3,
+        }}
+      >
+        <Grid item xs={8}>
+          <Divider
+            sx={{
+              mb: 2,
+              mr: 5,
+            }}
+          />
+          <Typography variant="h4" gutterBottom>
+            {" "}
+            Section Header
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+            molestias voluptates dolorum commodi consequuntur, doloremque
+            aperiam. Quisquam architecto dolorum velit.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+            explicabo harum delectus animi minima accusantium, veniam alias,
+            impedit officiis aliquid similique quod, consectetur aspernatur
+            numquam.
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+            repudiandae mollitia? Quas sed reiciendis, eligendi laborum enim
+            vitae sint nulla voluptatum, magni eaque exercitationem ut!
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            assumenda vel harum modi molestiae animi eligendi quam error,
+            aspernatur deserunt eum officia debitis. Itaque, a?
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            text
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
