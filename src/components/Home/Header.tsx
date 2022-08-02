e4fc0efaa445dd50cf23e8771e23172ec24b5fd9
k4-100 <k4-100@protonmail.com>
2022-08-02 20:50:42 +0200

    improved header's look

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,14 +1,34 @@
 import * as React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import BorderStyleIcon from "@mui/icons-material/BorderStyle";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar>
+      <AppBar sx={{ position: "static" }}>
         <Toolbar>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            SHEET
-          </Typography>
+          <Button
+            component={RouterLink}
+            to="/"
+            sx={{
+              mr: "auto",
+              display: "flex",
+            }}
+          >
+            <BorderStyleIcon
+              sx={{
+                mr: 1,
+              }}
+            />
+            <Typography variant="h5">Sheet</Typography>
+          </Button>
+          <Button component={RouterLink} to="/">
+            Home
+          </Button>
+          <Button component={RouterLink} to="/calc">
+            Calc
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
