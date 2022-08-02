b194c6bc4f3e973e0899a9c432293abf42bbf39b
k4-100 <k4-100@protonmail.com>
2022-08-02 19:47:54 +0200

    Changed app to use black theme by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "./dist/css/compiled/main.css";
-import Sheet from "./components/Sheet";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sheet from "./components/Sheet";
 import Home from "./components/Home";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="calc" element={<Sheet />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="calc" element={<Sheet />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 };
 
--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,27 +1,17 @@
-import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { AiOutlineCalculator } from "react-icons/ai";
-import { CgNotes, CgImage } from "react-icons/cg";
+import * as React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
 const Header = () => {
   return (
-    <header className="fixed-top">
-      <Navbar className="px-3 py-0" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">SHEET</Navbar.Brand>
-        <div className="vr text-light px-3 "></div>
-        <Nav className="me-auto">
-          <Link className="nav-link p-0 px-1" to="/calc">
-            <AiOutlineCalculator className="h2 h-100" />
-          </Link>
-          <Link className="nav-link p-0 px-1" to="/calc">
-            <CgNotes className=" h1 h-100" />
-          </Link>
-          <Link className="nav-link p-0 px-1" to="/calc">
-            <CgImage className="h1 h-100" />
-          </Link>
-        </Nav>
-      </Navbar>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            SHEET
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
