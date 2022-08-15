fba9b2d10a89508b48e99e0c7119f9b20f8fcc5c
k4-100 <k4-100@protonmail.com>
2022-08-15 20:37:28 +0200

    Put SliderCard into a separate component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,5 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  Divider,
-  Grid,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -19,48 +9,7 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 import Header from "./Header";
-import { AccountCircle } from "@mui/icons-material";
-
-const SliderCard: React.FC = () => {
-  return (
-    <Card>
-      <CardActionArea>
-        <CardContent
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            "& > *": {
-              flex: 1,
-            },
-          }}
-        >
-          <Box
-            sx={{
-              height: "25px",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <AccountCircle />
-            <Typography>John White</Typography>
-          </Box>
-          <Typography
-            variant="body1"
-            sx={{
-              my: 1,
-            }}
-          >
-            Ciq ipsum dolor sit amet, consectetur adipisicing elit. Quidem odit
-            perspiciatis nesciunt doloremque cumque.!
-          </Typography>
-          <Typography variant="subtitle2" color="text.secondary">
-            CEO at Company.com
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
-};
+import SliderCard from "./SliderCard";
 
 /**
  *
