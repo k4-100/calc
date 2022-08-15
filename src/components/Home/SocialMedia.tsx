6a4c39d24330614804737a55ebdf7bf97a3e3b6b
k4-100 <k4-100@protonmail.com>
2022-08-15 20:55:44 +0200

    Added bunch of links to SocialMedia

--- src/components/Home/SocialMedia.tsx
+++ src/components/Home/SocialMedia.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Paper, Button, Typography } from "@mui/material";
 import React from "react";
 
 /**
@@ -7,8 +7,44 @@ import React from "react";
  */
 const SocialMedia = () => {
   return (
-    <Paper elevation={10} sx={{ height: "200px", mt: 3 }}>
-      text here
+    <Paper
+      elevation={10}
+      sx={{
+        height: "200px",
+        mt: 3,
+        display: "flex",
+        flexDirection: "column",
+        "& > *": {
+          p: 1,
+          fontSize: "20px",
+          fontWeight: "bold",
+        },
+        "& > *:not(:first-child)": {
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+        },
+        "& > *:not(:last-child)": {
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+        },
+      }}
+    >
+      <Button
+        href="https://www.facebook.com/"
+        variant="contained"
+        disableElevation
+      >
+        Facebook
+      </Button>
+      <Button href="https://www.facebook.com/" variant="contained">
+        Twitter
+      </Button>
+      <Button href="https://www.facebook.com/" variant="contained">
+        Instagram
+      </Button>
+      <Button href="https://www.facebook.com/" variant="contained">
+        Snapchat
+      </Button>
     </Paper>
   );
 };
