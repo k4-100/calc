f0c69724623598e5c1e0ca4336a9cdedf2c19369
k4-100 <k4-100@protonmail.com>
2022-08-18 20:28:12 +0200

    Improved header's responsibility

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -7,13 +7,25 @@ const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar sx={{ position: "static" }}>
-        <Toolbar>
+        <Toolbar
+          sx={{
+            flexDirection: {
+              sm: "row",
+              xs: "column",
+            },
+            alignItems: {
+              sm: "center",
+              xs: "flex-start",
+            },
+          }}
+        >
           <Button
             component={RouterLink}
             to="/"
             sx={{
               mr: "auto",
               display: "flex",
+              alignItems: "center",
             }}
           >
             <BorderStyleIcon
@@ -21,14 +33,24 @@ const Header = () => {
                 mr: 1,
               }}
             />
-            <Typography variant="h5">Sheet</Typography>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{
+                height: "25px",
+              }}
+            >
+              Sheet
+            </Typography>
           </Button>
-          <Button component={RouterLink} to="/">
-            Home
-          </Button>
-          <Button component={RouterLink} to="/calc">
-            Calc
-          </Button>
+          <Box>
+            <Button component={RouterLink} to="/">
+              Home
+            </Button>
+            <Button component={RouterLink} to="/calc">
+              Calc
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
