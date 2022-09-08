f8611915b6dafe1a15db2147ddb6a0ebcaead101
k4-100 <k4-100@protonmail.com>
2022-09-08 17:46:32 +0200

    Added caret position correction

--- src/components/Sheet/CustomTable/CustomTableCell.tsx
+++ src/components/Sheet/CustomTable/CustomTableCell.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import _ from "lodash";
 import { TableCell } from "@mui/material";
 import { CellClass } from "../../../utility/Classes";
@@ -19,7 +19,48 @@ const CustomTableCell: React.FC<Props> = ({
 }) => {
   const [text, setText] = useState<string>("");
   const [clicks, setClicks] = useState<number>(0);
+  const tdRef = useRef<any>(null);
 
+  const placeCaretAtEnd = (el: any) => {
+    // el.focus();
+    // if (
+    //   typeof window.getSelection != "undefined" &&
+    //   typeof document.createRange != "undefined"
+    // ) {
+    // if()
+    const range = document.createRange();
+    range.selectNodeContents(el);
+    range.collapse(false);
+    const sel: any = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    // }
+    // else if (typeof (<any>document.body).createTextRange != "undefined")      {
+    // var textRange = document.createTextRange();
+    //   textRange.moveToElementText(el);
+    //   textRange.collapse(false);
+    //   textRange.select();
+    // }
+  };
+
+  useEffect(() => {
+    // if (tdRef !== null) {
+    const { current } = tdRef as any;
+    const handleDOMSubtreeModified = () => {
+      const { textContent } = tdRef.current;
+      // tdRef.current.selectionStart = textContent.length;
+      placeCaretAtEnd(tdRef.current);
+      setText(textContent);
+      console.log("textModified", textContent);
+    };
+    current.addEventListener("DOMSubtreeModified", handleDOMSubtreeModified);
+    return () =>
+      current.removeEventListener(
+        "DOMSubtreeModified",
+        handleDOMSubtreeModified
+      );
+    // }
+  }, []);
   /**
    *
    * @param x horizontal (column/cell) cell coords
@@ -30,6 +71,12 @@ const CustomTableCell: React.FC<Props> = ({
     if (clicks < 2) setClicks(clicks + 1);
   };
 
+  // useEffect(() => {
+  //   const cellDOM: any = document.querySelector(`#td-${x}-${y}`);
+  //   if (cellDOM && text !== cellDOM.textContent) {
+  //     setTimeout(() => setText(() => cellDOM.textContent), 1);
+  //   }
+  // }, [text]);
   /**
    *
    * @param x horizontal (column/cell) cell coords
@@ -43,19 +90,27 @@ const CustomTableCell: React.FC<Props> = ({
       if (keyCode === 13) {
         e.preventDefault();
 
-        cloneAndSetTableCell(x, y, (cl: CellClass) => {
-          const _cell = _.cloneDeep(cl);
-          if (clicks === 2) {
-            cell.text = e.target.textContent;
-            cell.value = getEvaluatedText(e.target.textContent);
-            setClicks(0);
-          }
-        });
+        // cloneAndSetTableCell(x, y, (cl: CellClass) => {
+        if (clicks === 2) {
+          setText(e.target.textContent);
+          // value = getEvaluatedText(e.target.textContent);
+          setClicks(0);
+        }
+        // });
+      } else {
+        // on non-ENTER key press if cell was clicked on once
+        if (clicks < 2) {
+          setClicks(2);
+        }
+        // console.log(e);
+        const { textContent } = tdRef.current as any;
+        const cellDOM: any = document.querySelector(`#${e.target.id}`);
+        setText(() => cellDOM.textContent);
       }
-      // on non-ENTER key press if cell was clicked on once
-      else {
-        if (clicks < 2) setClicks(2);
-      }
+
+      // console.log(e.target.textContent);
+      // console.log(e.target);
+      // console.log(e);
     },
     [cloneAndSetTableCell, getEvaluatedText]
   );
@@ -81,11 +136,12 @@ const CustomTableCell: React.FC<Props> = ({
   return (
     <TableCell
       key={x}
+      ref={tdRef}
       id={`td-${x}-${y}`}
       contentEditable
       suppressContentEditableWarning
       // onBlur={(e) => handleCellBlur(x, y, e)}
-      // onKeyDown={(e) => handleCellKeyDown(x, y, e)}
+      onKeyDown={(e) => handleCellKeyDown(x, y, e)}
       // onClick={(e) => handleCellClick(x, y, e)}
       sx={{
         // position: data.clicks === 0 ? "" : "absolute",
@@ -107,7 +163,8 @@ const CustomTableCell: React.FC<Props> = ({
         whiteSpace: "nowrap",
       }}
     >
-      {cell.clicks === 2 ? text : cell.value}
+      {text}
+      {/* {clicks === 2 ? text : cell.value} */}
     </TableCell>
   );
 };
