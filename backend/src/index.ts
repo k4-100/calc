743f89aacc03d6462a423213067f9398d96eff13
k4-100 <k4-100@protonmail.com>
2022-10-16 10:15:40 +0200

    Added basic query

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,36 @@
 import express from "express";
+import mysql from "mysql";
+import _ from "lodash";
+
 const app = express();
 
 const PORT: number = 5000;
 const HOSTNAME: string = "127.0.0.1";
 
-app.get("/", (req, res) => {
-  res.status(200).send("HOME");
+const mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "Products",
+});
+
+mysqlConnection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+app.get("/", async (req, res) => {
+  const p = new Promise((res, rej) => {
+    mysqlConnection.query("SELECT * FROM Products.Food", (err, result) => {
+      if (err) throw err;
+
+      res({ result });
+    });
+  }).catch((err) => console.log("promise error: ", err));
+
+  const ret: any = await p;
+  console.log(ret);
+  return res.json({ data: ret.result, msg: "stuff", status: true });
 });
 
 app.all("*", (req, res) => {
