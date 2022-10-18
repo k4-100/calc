5a74d41f11fcc26896f5e8eec19043236c99af90
k4-100 <k4-100@protonmail.com>
2022-10-18 21:42:06 +0200

    Barebone Profile styling

--- ca/src/components/Home/Profile.tsx
+++ ca/src/components/Home/Profile.tsx
@@ -1,14 +1,41 @@
-import { Paper } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 import React from "react";
 
 const Profile = () => {
   return (
     <Paper
+      elevation={5}
       sx={{
         position: "absolute",
+        display: "flex",
+        flexDirection: "column",
+        padding: 1,
+        top: "75px",
+        right: "20px",
+        "& > *:nth-of-type(1)": {
+          mt: 0,
+        },
+        "& > *:nth-of-type(2)": {
+          my: 1,
+        },
       }}
     >
-      sda
+      <TextField
+        id="profile-login"
+        label="Login"
+        variant="outlined"
+        size="small"
+      />
+      <TextField
+        id="profile-login"
+        label="Password"
+        variant="outlined"
+        size="small"
+      />
+      <Button variant="contained" color="success">
+        Log in
+      </Button>
+      <Button>Register</Button>
     </Paper>
   );
 };
