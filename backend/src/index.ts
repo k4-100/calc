300bb6ae6980651a19fc3dfd77ccb44c02ac4710
k4-100 <k4-100@protonmail.com>
2022-11-04 19:47:05 +0100

    Fixed res({result}) firing on error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import mysql from "mysql";
 import _ from "lodash";
 
@@ -19,12 +19,17 @@ mysqlConnection.connect((err) => {
   console.log("Connected");
 });
 
-app.get("/api/v1/users", async (_, res) => {
+app.get("/login", async (req, res) => {
   let error = false;
-  const p = new Promise((res, rej) => {
-    mysqlConnection.query("SELECT * FROM Users", (err, result) => {
-      if (err) rej(err);
+  const { id, username, pass } = req.query;
+  if (!id || !username || !pass)
+    return res.status(400).json({ data: {}, status: false });
 
+  const p = new Promise((res, rej) => {
+    mysqlConnection.query("SELECT * FRO Users", (err, result) => {
+      if (err) return rej(err);
+
+      res({ result });
       res({
         result: result.map(({ userID, username }: any) => ({
           userID,
@@ -39,10 +44,33 @@ app.get("/api/v1/users", async (_, res) => {
 
   const ret: any = await p;
   console.log(ret);
-  if (!error) return res.status(201).json({ data: ret.result, status: true });
-
-  return res.status(404).json({ data: [], status: false });
+  if (error) return res.status(404).json({ data: [], status: false });
+  return res.status(201).json({ data: ret.result, status: true });
 });
+
+// app.get("/api/v1/users", async (_, res) => {
+//   let error = false;
+//   const p = new Promise((res, rej) => {
+//     mysqlConnection.query("SELECT * FROM Users", (err, result) => {
+//       if (err) rej(err);
+
+//       res({
+//         result: result.map(({ userID, username }: any) => ({
+//           userID,
+//           username,
+//         })),
+//       });
+//     });
+//   }).catch((err) => {
+//     console.log("promise error: ", err);
+//     error = true;
+//   });
+
+//   if (!error) return res.status(404).json({ data: [], status: false });
+
+//   const ret: any = await p;
+//   return res.status(201).json({ data: ret.result, status: true });
+// });
 
 app.all("*", (req, res) => {
   res.status(404).send("ERROR");
