2b0dcd270233f70c73a9806a00d7319ff60952b3
k4-100 <k4-100@protonmail.com>
2023-03-04 15:13:31 +0100

    Basic implementation of localStorage

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,70 +1,37 @@
 import { configureStore, createSlice, createReducer } from "@reduxjs/toolkit"
 import produce, { current } from 'immer'
 import _ from "lodash";
-import { SheetClass, CellClass, TableClass } from '../utility/Classes'
+import { SheetClass, CellClass, TableClass, SheetClassObjectType } from '../utility/Classes'
 
 
-// const cl = new CellClass(1,2,"text");
-// console.log( cl )
-// console.log( cl.getObject() )
-// const tb = new TableClass(5,3, "213")
-// console.log( tb )
-// console.log( tb.getObject() )
-// const sh = new SheetClass()
-// console.log( sh )
-// console.log( sh.getObject() )
+const newSheet: SheetClassObjectType = new SheetClass().getObject();
+// localStorage.setItem('sheet', JSON.stringify(newSheet) );
 
+
+const determineInitialState = (): SheetClassObjectType => {
+  const sheetRaw: null | string = localStorage.getItem('sheet');
+  if( !sheetRaw ){
+    const newSheet: SheetClassObjectType = new SheetClass().getObject();
+    localStorage.setItem('sheet', JSON.stringify(newSheet) );
+
+    return newSheet;
+  }
+  
+  return JSON.parse(sheetRaw) as SheetClassObjectType;
+}
 
 const calcSlice = createSlice({
   name: 'calc',
-  initialState: new SheetClass().getObject(),
+  initialState: determineInitialState(),
   reducers: {
     setSheet(state, action){
-      // console.log( "state: ", current( state ))
-      // console.log( "action.payload: ", action.payload )
-      // const newSheets = _.cloneDeep( state )
-      // newSheets.id = '3214124'
+      
+      localStorage.setItem('sheet', JSON.stringify(action.payload) );
       return action.payload
-      // newSheets.id = "sar"
-      // return action.payload
-      // return {
-      //   ...state,
-      //   sheets: {
-      //     ...state.sheets,
-      //     // ssa: new SheetClass().getObject
-      //     tables: state.sheets.tables.map( table => {
-      //       return ({
-      //         ...table,
-      //         cells: table.cells.map( 
-      //           row => row.map( cell => ({...cell}) ) 
-      //         )
-      //       })
-      //     } )
-      //   }
-      //   
-      // }
 
     }
-    // increment(state) {
-    //   state.counter++
-    // },
-    // decrement(state) {
-    //   state.counter--
-    // },
-    // addBy    (state, action) {
-    //   state.counter += action.payload
-    // },
   }
 })
-
-
-
-// const todosReducer = createReducer([], (builder) => {
-//   builder.addCase('todos/todoAdded', (state, action) => {
-//     // "mutate" the array by calling push()
-//     state.push(action.payload)
-//   })
-// })
 
 
 export const actions = calcSlice.actions;
