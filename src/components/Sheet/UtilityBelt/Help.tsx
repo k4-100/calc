a74fa35b3cee4ac793c18e934e779eeaf19eb6e8
k4-100 <k4-100@protonmail.com>
2023-03-31 16:43:23 +0200

    Added entries to help

--- src/components/Sheet/UtilityBelt/Help.tsx
+++ src/components/Sheet/UtilityBelt/Help.tsx
@@ -1,85 +1,120 @@
 // !
 import React from "react";
 import { Box, IconButton, List, ListItem, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
-import { Cancel } from "@mui/icons-material";
+import { grey, red } from "@mui/material/colors";
+import { Cancel, LooksOne } from "@mui/icons-material";
 
 interface Props {
-  handleXClick: () => void;
+    handleXClick: () => void;
 }
 
 const Help = ({ handleXClick }: Props) => {
-  return (
-    <Box
-      sx={{
-        backgroundColor: "hsla(0,0%,10%,0.7)",
-        zIndex: 1000,
-        position: "fixed",
-        top: 0,
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <Box
-        sx={{
-          height: "75%",
-          mt: 10,
-          mx: 15,
-          backgroundColor: grey[900],
-        }}
-      >
+    return (
         <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            p: 2,
-            py: 3,
-          }}
+            sx={{
+                backgroundColor: "hsla(0,0%,10%,0.7)",
+                zIndex: 1000,
+                position: "fixed",
+                top: 0,
+                width: "100%",
+                height: "100%",
+            }}
         >
-          <Typography variant="h4">How to Guide:</Typography>
-          <IconButton onClick={handleXClick}>
-            <Cancel fontSize="large" color="error" />
-          </IconButton>
+            <Box
+                sx={{
+                    height: "75%",
+                    mt: 10,
+                    mx: 15,
+                    backgroundColor: grey[900],
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        p: 2,
+                        py: 3,
+                    }}
+                >
+                    <Typography variant="h4">How to Guide:</Typography>
+                    <IconButton onClick={handleXClick}>
+                        <Cancel fontSize="large" color="error" />
+                    </IconButton>
+                </Box>
+                <List
+                    sx={{
+                        mx: 3,
+                        backgroundColor: "white",
+                        color: grey[800],
+                        borderRadius: "10px",
+                        "& > *": {
+                            display: "flex",
+                            // alignItems: "center",
+                            border: `1px solid ${grey[200]}`,
+                            minHeight: "75px",
+                            fontSize: "19px",
+                        },
+                        "& > *:first-of-type": {
+                            borderTop: "0",
+                        },
+                        "& > *:last-of-type": {
+                            borderBottom: "0",
+                        },
+                    }}
+                >
+                    <ListItem>
+                        To change text, click twice on a cell and start typing,
+                        and save by clicking Enter/clicking somewhere else.
+                    </ListItem>
+                    <ListItem>
+                        To perform calculation, begin by typing `=`, for
+                        example: =10-5
+                    </ListItem>
+                    <ListItem>
+                        You can access cell for use in mathematical expression
+                        by its "name" (for example: A1, B3, C2), by typing `=`
+                        before it (like: =A2, =B3).
+                    </ListItem>
+                    <ListItem>
+                        To change current profile/sheet, press the number in
+                        header and switch to another one.
+                    </ListItem>
+                    <ListItem
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                        }}
+                    >
+                        You can use functions operating on numbers in cells:
+                        <Typography>
+                            Sum(x:y) {"<"}- sums up numbers in x:y range (for
+                            example: Sum(A1:B2) )
+                        </Typography>
+                        <Typography>
+                            Avg(x:y) {"<"}- returns an average of numbers in x:y
+                            range (for example: Avg(E4:H10) )
+                        </Typography>
+                    </ListItem>
+                    <ListItem>
+                        Exit this menu by pressing{" "}
+                        <Cancel
+                            fontSize="large"
+                            color="error"
+                            sx={{
+                                mx: 1,
+                                backgroundColor: `${grey[800]}`,
+                                color: `${red[500]}`,
+                                fontSize: "35px",
+                                borderRadius: "100%",
+                            }}
+                        />
+                        in a top-right corner.
+                    </ListItem>
+                </List>
+            </Box>
         </Box>
-        <List
-          sx={{
-            mx: 3,
-            backgroundColor: "white",
-            color: grey[800],
-            borderRadius: "10px",
-            "& > *": {
-              display: "flex",
-              // alignItems: "center",
-              border: `1px solid ${grey[200]}`,
-              height: "75px",
-              fontSize: "19px",
-            },
-            "& > *:first-of-type": {
-              borderTop: "0",
-            },
-            "& > *:last-of-type": {
-              borderBottom: "0",
-            },
-          }}
-        >
-          <ListItem>
-            To change text, click twice on a cell and start typing, and save by
-            clicking Enter/clicking somewhere else.
-          </ListItem>
-          <ListItem>
-            To perform calculation, begin by typing `=`, for example: =10-5
-          </ListItem>
-          <ListItem>
-            You can access cell for use in mathematical expression by its "name"
-            (for example: A1, B3, C2), by typing `=` before it (like: =A2, =B3).
-          </ListItem>
-          <ListItem>
-            Exit this menu by pressing X in a top-right corner.{" "}
-          </ListItem>
-        </List>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default Help;
