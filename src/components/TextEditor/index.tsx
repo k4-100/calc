cdf5796472a4203079bd7e379783cfaeb9224b14
k4-100 <k4-100@protonmail.com>
2023-04-25 15:57:43 +0200

    Added text section to textEditor

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,8 +1,60 @@
-import { Box } from "@mui/system";
+import { grey } from "@mui/material/colors";
+import { Box, Paper, Typography } from "@mui/material";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
 import React from "react";
+import UtilityBelt from "../common/UtilityBelt";
 
 const TextEditor: React.FC = () => {
-    return <Box>text is here</Box>;
+    return (
+        <Box
+            sx={{
+                height: "100vh",
+                maxHeight: "100vh",
+            }}
+        >
+            <UtilityBelt />
+            <Box
+                sx={{
+                    margin: 2,
+                    height: "100%",
+                    maxHeight: "85vh",
+                }}
+            >
+                <Paper
+                    sx={{
+                        boxSizing: "border-box",
+                        width: "50%",
+                        height: "100%",
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            // px: 1,
+                            pl: "10px",
+                            py: 0.5,
+
+                            color: grey[600],
+                        }}
+                    >
+                        text
+                    </Typography>
+                    <TextareaAutosize
+                        aria-label="empty textarea"
+                        placeholder="Empty"
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            backgroundColor: grey[800],
+                            color: "white",
+                            padding: "10px",
+                            fontSize: "18px",
+                        }}
+                    />
+                </Paper>
+            </Box>
+        </Box>
+    );
 };
 
 export default TextEditor;
