9328f343161b8e8c1dc8e37ecccaf2f9e731e86d
k4-100 <k4-100@protonmail.com>
2023-11-03 19:09:17 +0100

    removed unneccesary debugger;

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ const App: React.FC = () => {
                 token.username === "" &&
                 token.accesstoken === ""
             ) {
-                debugger;
                 dispatch(
                     actions.setAccessToken({
                         id: userid,
